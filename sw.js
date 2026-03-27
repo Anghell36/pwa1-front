56ecec75be151d581f0f0dbd4116fc64e67bbf9d
Anghell36 <angelpit_@hotmail.com>
2026-03-27 12:59:03 -0600

    first commit

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'quesillo-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+  // Aquí puedes agregar rutas a tus imágenes o iconos locales si los descargas
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Caché abierto');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Interceptar peticiones (Estrategia Cache-First para recursos estáticos)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Si el archivo está en caché, lo devuelve. Si no, va a internet.
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Actualización del Service Worker (borra cachés viejos)
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
